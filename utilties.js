87d9dcb265dee750c116a666cee3967d9f6311e1
Niklas Fähnrich <niklas.fahnrich@safeture.com>
2024-12-03 14:40:26 +0100

    update example

--- utilties.js
+++ utilties.js
@@ -13,13 +13,37 @@ function createUserCard(user) {
   return card;
 }
 
+function createUserPage(user) {
+  const userPage = /*html*/ `
+    <div class="user-page">
+      <h3 class="name">${user.name}</h3>
+      <p class="username">username: ${user.username}</p>
+      <p class="phone">Phone: ${user.phone}</p>
+      <p class="email">Email: ${user.email}</p>
+      <div class="address">
+        <p>${user.address.city}</p>
+        <p>${user.address.street}</p>
+      </div>
+      <div class="actions">
+        <button>Back to user list</button>
+      </div>
+    </div>
+  `;
+
+  return userPage;
+}
+
 export async function getAllUsers() {
   const res = await fetch(baseURL + "/users");
   const users = await res.json();
   return users;
 }
 
-async function getUserById() {}
+async function getUserById(userId) {
+  const res = await fetch(baseURL + `/users/${userId}`);
+  const user = await res.json();
+  return user;
+}
 
 export function handleOnClick(event) {
   // const target = event.target;
@@ -27,6 +51,12 @@ export function handleOnClick(event) {
   const closetsCard = target.closest(".card");
 
   if (!closetsCard) return;
+
+  getUserById(closetsCard.id).then((user) => {
+    const userPageAsHtmlString = createUserPage(user);
+    const userList = document.querySelector(".user-list");
+    userList.innerHTML = userPageAsHtmlString;
+  });
 }
 
 export function insertUsersToDOM(users) {
